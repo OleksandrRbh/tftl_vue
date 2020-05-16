e8e2c01e7645b9c055b002e0d5831662130d694b
OleksandrRbh <OleksandrRbh@gmail.com>
2020-05-16 19:55:45 +0300

    fix deploy files

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        prependData: `@import "@/assets/styles/styles.scss";`
-      }
-    }
-  },
+module.exports = {  
   publicPath: process.env.NODE_ENV === 'production' ? '/tftl_vue/' : '/'
 }
